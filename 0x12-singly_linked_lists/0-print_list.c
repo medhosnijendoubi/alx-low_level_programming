55891e61c14512afd4b7209dccc1e6bcba2ebd53
medhosnijendoubi <mohamedhosnijendoubi@gmail.com>
2023-05-03 07:36:00 -0700

    add all files

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,17 +9,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t j = 0;
+	size_t s = 0;
 
 	while (h)
 	{
 		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %j\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		j++;
+		s++;
 	}
 
-	return (j);
+	return (s);
 }
