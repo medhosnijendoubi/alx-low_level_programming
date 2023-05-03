e246b224baeca2ed6a4107185697cdbd672c75cd
medhosnijendoubi <mohamedhosnijendoubi@gmail.com>
2023-05-03 07:57:54 -0700

    add all files

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+/**
+ * free_list - is a frees to a linkedlist
+ * @head: is a  list to bee freed
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
