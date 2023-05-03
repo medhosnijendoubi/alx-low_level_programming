6f061b9a9acb49ca05980231ca626d75c5dca4ca
medhosnijendoubi <mohamedhosnijendoubi@gmail.com>
2023-05-03 07:55:54 -0700

    add all files

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,17 +13,17 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
 	list_t *temp = *head;
-	unsigned int test = 0;
+	unsigned int len = 0;
 
-	while (str[test])
-		test++;
+	while (str[len])
+		len++;
 
 	new = malloc(sizeof(list_t));
 	if (!new)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->test = test;
+	new->len = len;
 	new->next = NULL;
 
 	if (*head == NULL)
